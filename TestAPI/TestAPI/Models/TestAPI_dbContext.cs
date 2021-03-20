17c830eb75b44b9047e7a9ed466346ab767f22f1
Jonathan <jonathanmcclellanddev@gmail.com>
2021-03-20 05:50:50 +0500

    First Commit

---
+++ TestAPI/TestAPI/Models/TestAPI_dbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TestAPI.Models
+{
+    public partial class TestAPI_dbContext : DbContext
+    {
+        public TestAPI_dbContext()
+        {
+        }
+
+        public TestAPI_dbContext(DbContextOptions<TestAPI_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Login> Login { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:testapidbserver1.database.windows.net,1433;Initial Catalog=TestAPI_db;Persist Security Info=False;User ID=wasif;Password=Dgr8samnick;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Username).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
